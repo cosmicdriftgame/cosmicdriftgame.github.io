5f07a85ebd5c6145889898234a38b0fdc641749a
cosmicdriftgame <117037900+cosmicdriftgame@users.noreply.github.com>
2022-10-30 21:15:47 +0200

    Initial commit

---
+++ consent.cookiebot.com/918cbc71-1c03-452f-964a-0a0560f9c749/cc.js
@@ -0,0 +1,8 @@
+if (console) {
+    var cookiedomainwarning = 'Error: The domain COSMICDRIFT.GAMES is not authorized to show the cookie banner for domain group ID 918cbc71-1c03-452f-964a-0a0560f9c749. Please add it to the domain group in the Cookiebot Manager to authorize the domain.';
+    if (typeof console.warn === 'function') {
+        console.warn(cookiedomainwarning)
+    } else {
+        console.log(cookiedomainwarning)
+    }
+};
